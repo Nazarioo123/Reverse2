507b9ced3f9dff4c6006f848ba5f9d9ed22b3164
Nazarii Lishchynskiy <132710119+Nazarioo123@users.noreply.github.com>
2025-01-23 17:32:40 +0100

    Add files via upload

---
+++ 312/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+       static void Main()
+
+        {
+         string s = Console.ReadLine();
+        char[] sReverse = s.ToCharArray();
+        Array.Reverse(sReverse);
+        s = new string(sReverse);
+        Console.WriteLine(s);
+        Console.ReadLine();
+         
+
+
+        }
+  
+}
